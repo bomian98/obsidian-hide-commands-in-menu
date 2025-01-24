dcfe5acf8d411a9393feea066c25bba75003a70e
bomian98 <1998wsc@gmail.com>
2025-01-24 09:30:04 +0800

    feat: remove add-command feature, optimize remove-command feature. In plugin setting tab, one command per line for removal, not comma-separated.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Command, Plugin, MenuItem } from 'obsidian';
+import { Plugin, MenuItem } from 'obsidian';
 import CustomMenuSettingsTab, { CustomMenuSettings, DEFAULT_SETTINGS } from './ui/settingsTab';
 import { around } from 'monkey-around';
 
@@ -11,53 +11,20 @@ export default class CustomMenuPlugin extends Plugin {
 		await this.loadSettings();
 		this.addSettingTab(new CustomMenuSettingsTab(this.app, this));
 
-		this.settings.menuCommands.forEach(command => {
-			this.addMenuItem(command);
-		});
-
-		/* moneky-around doesn't know about my this.settings, need to set it here */
 		let hideTitles = this.settings.hideTitles
+		// console.log("hideTitles", hideTitles)
 
-		/* Hide menu items */
-		/* https://github.com/Panossa/mindful-obsidian/blob/master/main.ts */
 		this.register(around(MenuItem.prototype, {
 			setTitle(old) {
 				return function (title: string | DocumentFragment) {
 					this.dom.dataset.stylizerTitle = String(title);
-
 					if (hideTitles.includes(String(title))) {
 						this.dom.addClass('custom-menu-hide-item');
 					}
-
 					return old.call(this, title);
 				};
 			}
 		}));
-	}
-
-	//add command to right-click menu
-	addMenuItem(command: Command) {
-		this.registerEvent(
-			this.app.workspace.on("editor-menu", (menu) => {
-				menu.addItem((item) => {
-					item.setTitle(command.name)
-					.setIcon(command.icon)
-					.onClick(() => {
-						//@ts-ignore
-						this.app.commands.executeCommandById(command.id);
-					});
-				});
-			})
-		);
-	}
-
-	//add command to the list of commands to be added to right-click menu (persistent, saved in settings)
-	async addMenuItemSetting(command: Command, settingTab: CustomMenuSettingsTab) {
-		this.addMenuItem(command);
-		this.settings.menuCommands.push(command);
-		await this.saveSettings();
-
-		settingTab.display(); //refresh settings tab
 	}
 
 	async loadSettings() {
--- src/ui/settingsTab.ts
+++ src/ui/settingsTab.ts
@@ -1,16 +1,12 @@
-import { PluginSettingTab, App, Setting, setIcon, Command, Notice, debounce } from "obsidian";
+import { PluginSettingTab, App, Setting, debounce } from "obsidian";
 import CustomMenuPlugin from "src/main";
-import CommandSuggester from "./commandSuggester";
-import IconPicker from "./iconPicker";
 
 
 export interface CustomMenuSettings {
-    menuCommands: Command[];
     hideTitles: string[];
 }
 
 export const DEFAULT_SETTINGS: CustomMenuSettings = {
-    menuCommands: [],
     hideTitles: [],
 }
 
@@ -26,53 +22,14 @@ export default class CustomMenuSettingsTab extends PluginSettingTab {
     display(): void {
         let { containerEl } = this;
         containerEl.empty();
-        containerEl.createEl('h2', { text: 'Add commands' });
 
-        new Setting(containerEl)
-            .setName("Add command to menu")
-            .setDesc("Add a new command to the right-click menu")
-            .addButton((button) => {
-                button.setButtonText("Add Command")
-                    .onClick(() => {
-                        new CommandSuggester(this.plugin, this).open();
-                    });
-            });
-
-        this.plugin.settings.menuCommands.forEach(command => {
-            const iconDiv = createDiv({ cls: "custom-menu-settings-icon" });
-            setIcon(iconDiv, command.icon, 20);
-            const setting = new Setting(containerEl)
-                .setName(command.name)
-                .addExtraButton(button => {
-                    button.setIcon("trash")
-                        .setTooltip("Remove command")
-                        .onClick(async () => {
-                            this.plugin.settings.menuCommands.remove(command);
-                            await this.plugin.saveSettings();
-                            this.display();
-                            new Notice("You will need to restart Obsidian for the command to disappear.")
-                        })
-                })
-                .addExtraButton(button => {
-                    button.setIcon("gear")
-                        .setTooltip("Edit icon")
-                        .onClick(() => {
-                            new IconPicker(new CommandSuggester(this.plugin, this), command, true).open(); //rewrite icon picker so it isn't taking a command suggester
-                        })
-                });
-            setting.nameEl.prepend(iconDiv);
-            setting.nameEl.addClass("custom-menu-flex");
-        });
-
-        /* Hide commands */
         containerEl.createEl('h2', { text: 'Hide commands' });
 
-        // https://github.com/ozntel/file-explorer-note-count/blob/main/src/settings.ts#L117=
         new Setting(containerEl)
             .setDesc("Enter the names of the commands as a comma-separated list. Commands are case-sensitive. You will need to restart Obsidian for the changes to take effect.")
             .addTextArea(text => {
                 const onChange = async (value: string) => {
-                    const list = value.split(',').map((v) => v.trim());
+                    const list = value.split('\n').map((v) => v.trim());
                     this.plugin.settings.hideTitles = list;
                     await this.plugin.saveSettings();
                 };
@@ -80,9 +37,9 @@ export default class CustomMenuSettingsTab extends PluginSettingTab {
                     'Enter commands to hide',
                 );
                 text.setValue(
-                    this.plugin.settings.hideTitles.join(', '),
+                    this.plugin.settings.hideTitles.join('\n'),
                 ).onChange(debounce(onChange, 500, true));
-                text.inputEl.rows = 5;
+                text.inputEl.rows = 10;
                 text.inputEl.cols = 30;
             });
     }
