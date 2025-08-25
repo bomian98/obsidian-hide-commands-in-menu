d11c006ec43a1a53e7daa93bd089b8b6b3d7cbbd
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-08-25 14:42:20 +0200

    fix: Hide multiple consecutive separators if no items in between are visible

--- src/utils.ts
+++ src/utils.ts
@@ -95,13 +95,17 @@ export function hideMenuItems(
     ),
   ].map((regexStr) => new RegExp(regexStr));
 
+  // Tracks whether any non-hidden non-separator items
+  // have occured since the last separator.
+  let anyVisibleItemsSinceLastSeparator = false;
+  let lastSeparator: Element | null = null;
+
   // Handle menu items and separators hiding logic
   const visitChildrenOf = (container: Element) => {
     const children = Array.from(container.children);
     const totalChildren = children.length;
-    let lastSeparatorIdx = -1;
 
-    children.forEach((child, index) => {
+    children.forEach((child) => {
       if (child.classList.contains('menu-item')) {
         const titleElement = child.querySelector('.menu-item-title');
         const title = titleElement?.textContent?.trim();
@@ -112,31 +116,25 @@ export function hideMenuItems(
           regexTexts.some((regex) => regex.test(title));
         if (shouldHide) {
           child.classList.add('custom-menu-hide-item');
+        } else {
+          anyVisibleItemsSinceLastSeparator = true;
         }
       } else if (child.classList.contains('menu-separator')) {
-        if (shouldHideSeparator(children, lastSeparatorIdx, index)) {
+        if (anyVisibleItemsSinceLastSeparator) {
+          anyVisibleItemsSinceLastSeparator = false;
+        } else {
           child.classList.add('custom-menu-hide-separator');
         }
-        lastSeparatorIdx = index;
+        lastSeparator = child;
       } else if (child.classList.contains("menu-group")) {
         visitChildrenOf(child);
       }
     });
+  }
 
-    // Handle the last separator
-    if (shouldHideSeparator(children, lastSeparatorIdx, totalChildren)) {
-      children[lastSeparatorIdx].classList.add('custom-menu-hide-separator');
-      // Hide the separator if menu-items behind are all hidden
-      for (let i = lastSeparatorIdx - 1; i >= 0; i--) {
-        if (children[i].classList.contains('menu-item')) {
-          if (!children[i].classList.contains('custom-menu-hide-item')) {
-            break;
-          }
-        } else if (children[i].classList.contains('menu-separator')) {
-          children[i].classList.add('custom-menu-hide-separator');
-        }
-      }
-    }
+  // Handle the last separator
+  if (!anyVisibleItemsSinceLastSeparator && lastSeparator) {
+    lastSeparator.classList.add('custom-menu-hide-separator');
   }
 
   visitChildrenOf(menuContainer);
